f6f7adc9837d0afe413046b83326884584ee1c99
Syed Daanish Ismail <sismai20@uic.edu>
2026-02-12 18:21:14 -0600

    Add Goal Timer

--- Assignment2/Assets/Scripts/Target.cs
+++ Assignment2/Assets/Scripts/Target.cs
@@ -1,33 +1,96 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-// adding namespaces
 using Unity.Netcode;
 
 public class Target : NetworkBehaviour
 {
+    [Header("Timer")]
+    [SerializeField] private float timeLimit = 10f;
+    [SerializeField] private float moveStartDistance = 0.2f;
+    [SerializeField] private float minMoveSpeed = 0.1f;
 
-    //this method is called whenever a collision is detected
-    private void OnCollisionEnter(Collision collision)
+    private Rigidbody rb;
+    private Vector3 spawnPosition;
+    private Quaternion spawnRotation;
+    private float timeRemaining;
+    private bool timerStarted;
+    private bool reachedGoal;
+
+    private void Awake()
     {
-
-        // printing if collision is detected on the console
-        Debug.Log("Collision Detected");
-
-        // if the collision is detected destroy the object
-        DestroyTargetServerRpc();
+        rb = GetComponent<Rigidbody>();
     }
 
-    // client can not spawn or destroy objects
-    // so we need to use ServerRpc
-    // we also need to add RequireOwnership = false
-    // because we want to destroy the object even if the client is not the owner
-    [ServerRpc(RequireOwnership = false)]
-    public void DestroyTargetServerRpc()
+    public override void OnNetworkSpawn()
     {
-        //despawn
-        GetComponent<NetworkObject>().Despawn(true);
-        //after collision is detected destroy the gameobject
-        Destroy(gameObject);
+        if (!IsServer) return;
+
+        spawnPosition = transform.position;
+        spawnRotation = transform.rotation;
+        timeRemaining = timeLimit;
+        timerStarted = false;
+        reachedGoal = false;
     }
+
+    private void Update()
+    {
+        if (!IsServer || reachedGoal) return;
+
+        if (!timerStarted && HasStartedMoving())
+        {
+            timerStarted = true;
+        }
+
+        if (!timerStarted) return;
+
+        timeRemaining -= Time.deltaTime;
+
+        if (timeRemaining <= 0f)
+        {
+            ResetToSpawn();
+        }
+    }
+
+    private bool HasStartedMoving()
+    {
+        float distFromSpawn = Vector3.Distance(transform.position, spawnPosition);
+        bool movedEnough = distFromSpawn >= moveStartDistance;
+        bool hasSpeed = rb != null && rb.linearVelocity.magnitude >= minMoveSpeed;
+        return movedEnough || hasSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!IsServer || reachedGoal) return;
+
+        if (other.CompareTag("Goal"))
+        {
+            reachedGoal = true;
+            timerStarted = false;
+        }
+    }
+
+    [ContextMenu("Reset Target To Spawn")]
+    public void ResetToSpawn()
+    {
+        if (!IsServer) return;
+
+        timerStarted = false;
+        reachedGoal = false;
+        timeRemaining = timeLimit;
+
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.position = spawnPosition;
+            rb.rotation = spawnRotation;
+        }
+        else
+        {
+            transform.position = spawnPosition;
+            transform.rotation = spawnRotation;
+        }
+    }
+
+    public float GetTimeRemaining() => timeRemaining;
 }
